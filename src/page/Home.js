0c8e489ab02cb497bfc551d97a39ae5efbea6536
thikajuniarti <juni4rt1@gmail.com>
2018-10-03 15:51:16 +0700

    pagination

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { List, Icon } from 'antd';
-import { Query } from "react-apollo";
+import { Query, ApolloConsumer } from "react-apollo";
 import gql from "graphql-tag";
 
 const listData = [];
@@ -24,39 +24,65 @@ const IconText = ({ type, text }) => (
 const DataList = () => (
   <Query
     query={gql`
-      {
-        me {
-          fullName
+      query users($cursor: String){
+        users(first: 20, after: $cursor) {
+          edges{
+            node {
+              fullName
+            }
+          }
+          pageInfo{
+						endCursor
+					}
         }
       }
     `}
   >
-    {({ loading, error, data }) => {
+    {({ loading, error, data: { users: users }, fetchMore}) => {
       if (loading) return <p>Loading...</p>;
       if (error) return `${error}`;
+      console.log(users);
       return(
         <List
           itemLayout="vertical"
           size="large"
           pagination={{
             onChange: (page) => {
-              console.log(page);
+              fetchMore({
+                variables: {
+                  cursor: users.pageInfo.endCursor
+                },
+                updateQuery: (previousResult, { fetchMoreResult }) => {
+                  const newEdges = fetchMoreResult.users.edges;
+                  const pageInfo = fetchMoreResult.users.pageInfo;
+
+                  return newEdges.length ?
+                    {
+                      users: {
+                        __typename: previousResult.users.__typename,
+                        edges: [...previousResult.users.edges, ...newEdges],
+                        pageInfo
+                      }
+                    } : previousResult
+                  console.log(newEdges);
+                }
+              })
             },
-            pageSize: 3,
+            pageSize: 10,
           }}
-          dataSource={[data.me]}
+          dataSource={users.edges}
           footer={<div><b>ant design</b> footer part</div>}
           renderItem={item => (
             <List.Item
-              key={item.fullName}
+              key={item.node.fullName}
               actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
               extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
             >
               <List.Item.Meta
-                title={<a href={item.fullName}>{item.fullName}</a>}
-                description={item.fullName}
+                title={<a href={item.node.fullName}>{item.node.fullName}</a>}
+                description={item.node.fullName}
               />
-              {item.fullName}
+              {item.node.fullName}
             </List.Item>
           )}
         />
@@ -65,10 +91,82 @@ const DataList = () => (
   </Query>
 );
 
+const DataList2 = (data) => {
+  console.log(data.data);
+  return <List
+    itemLayout="vertical"
+    size="large"
+    pagination={{
+      onChange: (page) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    }}
+    dataSource={data.data.users.edges}
+    footer={<div><b>ant design</b> footer part</div>}
+    renderItem={item => (
+      <List.Item
+        key={item.node.fullName}
+        actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+        extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+      >
+        <List.Item.Meta
+          title={<a href={item.node.fullName}>{item.node.fullName}</a>}
+          description={item.node.fullName}
+        />
+        {item.node.fullName}
+      </List.Item>
+    )}
+  />
+}
+
 export class Home extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      datauser: []
+    }
+  }
+
+  state = {
+    datalist: []
+  }
+
+  async componentDidMount(){
+    const data = await this.aclient.query({
+      query: gql`
+        query users($cursor: String){
+          users(first: 20, after: $cursor) {
+            edges{
+              node {
+                fullName
+              }
+            }
+            pageInfo{
+              endCursor
+            }
+          }
+        }
+      `
+    })
+    this.setState({
+      datauser: data
+    })
+  }
+
   render(){
+    // const datalist2 = ()
     return(
       <DataList />
+      // <div>
+      //   <ApolloConsumer>
+      //     {client => {
+      //       this.aclient = client;
+      //       return null;
+      //     }/* do stuff here */}
+      //   </ApolloConsumer>
+      //   <DataList2 data={this.state.datauser}/>
+      // </div>
     );
   }
 }
